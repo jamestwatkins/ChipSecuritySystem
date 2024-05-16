904d574f740810a15761279d203e295552d6b1df
james <jamestheodorewatkins@gmail.com>
2024-05-15 21:39:50 -0400

    Add project files.

---
+++ ChipValidationService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChipSecuritySystem
+{
+    public class ChipValidationService
+    {
+        //I decided I wanted to keep the Program file cleaner by seperating the validation logic into its own class
+        public static List<ColorChip> ValidateChips(List<ColorChip> chips)
+        {
+            List<ColorChip> currentSequence = new List<ColorChip>();
+
+            List<ColorChip> longestSequence = new List<ColorChip>();
+
+            List<int> usedIndexes = new List<int>();
+
+            //I wrote multiple checks to end the validation early if possible
+            if (chips == null || !chips.Any())
+            {
+                return null;
+            }
+            else if (!CheckForBlueStartAndGreenEnd(chips))
+            {
+                return null;
+            }
+            else if (chips.Count == 1 && chips[0].StartColor == Color.Blue && chips[0].EndColor == Color.Green)
+            {
+                return chips;
+            }
+            else if (chips.Count == 1)
+            {
+                return null;
+            }
+
+
+            return GetLongestValidSequence(chips, currentSequence, longestSequence, usedIndexes);
+        }
+        //If there isn't a chip that starts with blue and one that ends with green, the validation is guarenteed to fail. So I made that one of the checks
+        //I believe the time it takes to check each chip for this is worth it, considering how long the full validation takes
+        private static bool CheckForBlueStartAndGreenEnd(List<ColorChip> chips)
+        {
+            bool blueStart = false;
+            bool greenEnd = false;
+
+            foreach (ColorChip chip in chips)
+            {
+                if (chip.StartColor == Color.Blue)
+                {
+                    blueStart = true;
+                }
+
+                if (chip.EndColor == Color.Green)
+                {
+                    greenEnd = true;
+                }
+
+                if (blueStart && greenEnd)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        //For validating the chips, I used a permutation algorithm to check all possible ways the chips can be sorted
+        //As the algorithm goes on I save the longest valid sequence
+        //If I find a valid sequence that is the length of list, I end the validation early
+        //If not, I return the longest valid sequence at the end of the validation.
+        private static List<ColorChip> GetLongestValidSequence(List<ColorChip> chips, List<ColorChip> currentSequence, List<ColorChip> longestSequence, List<int> usedIndexes)
+        {
+            if (currentSequence.Count == chips.Count && currentSequence[currentSequence.Count() - 1].EndColor == Color.Green) 
+            {
+                longestSequence = currentSequence.ToList();
+
+                return longestSequence;
+            }
+            else if (currentSequence.Count > longestSequence.Count && currentSequence[0].StartColor == Color.Blue && currentSequence[currentSequence.Count - 1].EndColor == Color.Green)
+            {
+                longestSequence = currentSequence.ToList();
+            }
+
+            bool chipAdded = false;
+
+            for (int i = 0; i < chips.Count; i++)
+            {
+                if (!usedIndexes.Contains(i))
+                {
+                    if (!currentSequence.Any() && chips[i].StartColor == Color.Blue)
+                    {
+                        currentSequence.Add(chips[i]);
+
+                        chipAdded = true;
+                    }
+                    else if (currentSequence.Any() && currentSequence[currentSequence.Count - 1].EndColor == chips[i].StartColor)
+                    {
+                        currentSequence.Add(chips[i]);
+
+                        chipAdded = true;
+                    }
+
+                    if (chipAdded)
+                    {
+                        usedIndexes.Add(i);
+
+                        longestSequence = GetLongestValidSequence(chips, currentSequence, longestSequence, usedIndexes);
+
+                        if (longestSequence.Count == chips.Count)
+                        {
+                            break;
+                        }
+
+                        if (currentSequence.Any())
+                        {
+                            currentSequence.RemoveAt(currentSequence.Count - 1);
+                        }
+
+                        usedIndexes.Remove(i);
+                    }
+                }
+            }
+
+            return longestSequence;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChipSecuritySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I created multiple lists with different scenarios for testing.
+
+            List<ColorChip> multiValidList = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Orange),
+                new ColorChip(Color.Purple, Color.Green),
+                new ColorChip(Color.Yellow, Color.Purple),
+                new ColorChip(Color.Orange, Color.Red),
+                new ColorChip(Color.Red, Color.Yellow),
+                new ColorChip(Color.Yellow, Color.Red),
+                new ColorChip(Color.Green, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green)
+            };
+
+            List<ColorChip> noBlueStartList = new List<ColorChip>
+            {
+                new ColorChip(Color.Green, Color.Orange),
+                new ColorChip(Color.Purple, Color.Green),
+                new ColorChip(Color.Yellow, Color.Purple),
+                new ColorChip(Color.Orange, Color.Red),
+                new ColorChip(Color.Red, Color.Yellow),
+                new ColorChip(Color.Purple, Color.Green),
+                new ColorChip(Color.Green, Color.Red),
+                new ColorChip(Color.Red, Color.Green)
+            };
+
+            List<ColorChip> multiBlueList = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Orange),
+                new ColorChip(Color.Purple, Color.Green),
+                new ColorChip (Color.Blue, Color.Yellow),
+                new ColorChip(Color.Yellow, Color.Purple),
+                new ColorChip(Color.Orange, Color.Red),
+                new ColorChip(Color.Red, Color.Yellow),
+                new ColorChip(Color.Purple, Color.Green),
+                new ColorChip(Color.Green, Color.Red),
+                new ColorChip(Color.Red, Color.Green)
+            };
+
+            List<ColorChip> nonValidList = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Orange),
+                new ColorChip(Color.Purple, Color.Blue),
+                new ColorChip(Color.Yellow, Color.Purple),
+                new ColorChip(Color.Orange, Color.Red),
+                new ColorChip(Color.Red, Color.Yellow),
+                new ColorChip(Color.Purple, Color.Yellow),
+                new ColorChip(Color.Green, Color.Red),
+                new ColorChip(Color.Red, Color.Orange)
+            };
+
+            List<ColorChip> singleValidList = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Green)
+            };
+
+            List<ColorChip> singleInvalidList = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Orange)
+            };
+
+            List<ColorChip> emptyList = new List<ColorChip>();
+
+            //Call to validation service
+            var response = ChipValidationService.ValidateChips(multiValidList);
+
+            if (response != null && response.Any())
+            {
+                Console.WriteLine("Master Panel unlocked with the following chip(s):");
+
+                foreach (var chip in response)
+                {
+                    Console.WriteLine(chip.StartColor + " " + chip.EndColor);
+                }
+            }
+            else
+            {
+                Console.WriteLine(Constants.ErrorMessage);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
